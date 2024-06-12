b9a58bcc98ae4cbc563871a9bc2dc6deca745262
“210410204” <210410204@gmail.com>
2024-06-12 20:29:14 +0800

    all for vercel

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App_04 from './App_04.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App_04 />
+  </React.StrictMode>
+);
---
+++ src/pages/Dashboard_xx.jsx
@@ -0,0 +1,11 @@
+const Dashboard_xx = () => {
+  return (
+    <>
+      Guest name: wei chen <p></p>Guest student ID: 210410204<p></p>Supabase
+      email: 210410204@gmail.com<p></p> Supabase user id:
+      9ebd001f-b358-4cda-87f2-cb9a2e0833a7
+    </>
+  );
+};
+
+export default Dashboard_xx;
